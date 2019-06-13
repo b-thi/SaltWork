028bdbafddd8fb21539362be69f1becde0d65b71
Barinder <barinder.thi@gmail.com>
2019-06-12 21:20:57 -0700

    Just initial file

---
+++ IntroWork.py
@@ -0,0 +1,47 @@
+# Importing packages
+import os
+import sys
+import random
+import warnings
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#%matplotlib inline
+#%matplotlib auto
+%matplotlib qt
+import cv2
+
+from tqdm import tqdm_notebook, tnrange
+from itertools import chain
+from skimage.io import imread, imshow, concatenate_images
+from skimage.transform import resize
+from skimage.morphology import label
+
+from keras.models import Model, load_model
+from keras.layers import Input
+from keras.layers.core import Lambda
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras import backend as K
+
+import tensorflow as tf
+
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+
+# Data exploration
+ids= ['1f1cc6b3a4','5b7c160d0d','6c40978ddf','7dfdf6eeb8','7e5a6e5013']
+plt.figure(figsize=(20,10))
+for j, img_name in enumerate(ids):
+    q = j+1
+    img = load_img('C:/Users/Barinder/Documents/Salt Challenge/images/' + img_name + '.png')
+    img_mask = load_img('C:/Users/Barinder/Documents/Salt Challenge/masks/' + img_name + '.png')
+    
+    plt.subplot(1,2*(1+len(ids)),q*2-1)
+    plt.imshow(img)
+    plt.subplot(1,2*(1+len(ids)),q*2)
+    plt.imshow(img_mask)
+plt.show()
+
